eb95256faefd518e0d3ac5cf416ec098c471f7ef
SardorAxmadaliyev <168279312+SardorAxmadaliyev@users.noreply.github.com>
2024-08-03 21:29:31 +0500

    Update git_amaliyot.py
    komment qo'shildi

--- git_amaliyot.py
+++ git_amaliyot.py
@@ -1,3 +1,7 @@
+#"raqamlar” nomli tekst fayl ichiga 1 dan 10 gacha 
+sonlarni kiritib, ularning yig'indisini ekranga 
+chiqaruvchi dastur yarating.
+
 with open("raqamlar.txt","wt") as f:
     f.write("1 2 3 4 5 6 7 8 9 10")
 
