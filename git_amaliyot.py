645646329ee321591930d75efe84a9d2c57f025a
Sardor <Axmadaliyev Sardor396@gmail.com>
2024-08-03 20:56:20 +0500

    Masala yechildi va yangi fayl qo'shildi

---
+++ git_amaliyot.py
@@ -0,0 +1,12 @@
+with open("raqamlar.txt","wt") as f:
+    f.write("1 2 3 4 5 6 7 8 9 10")
+
+with open("raqamlar.txt","rt") as g:
+    data = g.read().split()
+    ls = []
+    for x in data:
+            ls.append(int(x))
+    sum = 0
+    for x in ls:
+        sum += x
+    print(sum)    
